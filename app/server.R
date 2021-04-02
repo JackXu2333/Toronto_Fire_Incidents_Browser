d01d417d818c587300605916b5bebd2ed360ef96
jackxu <sijie.xu@easygroup.ca>
2021-04-02 00:17:37 -0400

    updated interactive components

--- app/server.R
+++ app/server.R
@@ -1,21 +1,150 @@
 # Read fire Incidents file
-fireIncidents <- readRDS("../data/Fire_Incidents.Rds")
+fire_Incidents <- readRDS("../data/Trimmed_Fire_Incidents.Rds")
+fire_Stations <- readRDS("../data/Trimmed_Fire_Station.Rds")
+neighbourhood_Shape <- readRDS("../data/neighbourhoods_info.Rds")
+
 
 shinyServer(function(input, output) {
     
+    # Set global variable for plotting scatter plot / showing details
+    global_varible <- reactiveValues(fire_incidents_input = fire_Incidents, current_neibourhood = "", current_incidents = 0)
+    
     # Create the map
     output$map <- renderLeaflet({
-        leaflet(fireIncidents[1:10,]) %>% addProviderTiles("CartoDB.Positron") %>% 
-            addCircles(~Longitude, ~Latitude, radius=10, layerId=~X_id,
-                                       stroke=T, fillOpacity=0.4) %>%
-            setView(lng = -79.24, lat = 43.74, zoom = 10)
+        
+        # Set color factor
+        colorData <- fire_Incidents$Neighborhood
+        pal <- colorFactor("BuGn", colorData)
+        
+        leaflet(fire_Stations) %>% addProviderTiles("CartoDB.Positron") %>% 
+            setView(lng = -79.24, lat = 43.74, zoom = 11)  
     })
     
+    # This observer is responsible for maintaining the circles and legend,
+    # according to the variables the user has chosen to map to color and size.
+    observeEvent(input$Refresh,{
+        
+        Fire_Type <- input$Fire_Type %>% as.integer()
+        Fire_Size <- input$Fire_Size %>% as.integer()
+        Area_Origin <- input$Area_Origin %>% as.integer()
+        Financial_Loss <- input$Financial_Loss
+        Casualties <- input$Casualties
+        Num_Personnel <- input$Num_Personnel
+        Time <- input$Time
+        
+        # filter continues variables
+        fire_Incidents_Filtered <- fire_Incidents %>% 
+            filter(Estimated_Dollar_Loss >= Financial_Loss[1] & Estimated_Dollar_Loss <= Financial_Loss[2]) %>% 
+            filter(Civilian_Casualties <= Casualties) %>% 
+            filter(Number_of_responding_personnel >= Num_Personnel[1] & Number_of_responding_personnel <= Num_Personnel[2]) %>%
+            filter(TFS_Alarm_Date >= Time[1] & TFS_Alarm_Date <= Time[2])%>% 
+            filter(Fire_Type_Case %in% fire_Type_Table[Fire_Type])%>% 
+            filter(Fire_Size_Case %in% fire_Size_Table[Fire_Size])%>% 
+            filter(Area_Orgin_Case %in% area_Origin_Table[Area_Origin])
+        
+        # Add current filtering to the gv
+        global_varible$fire_incidents_input <- fire_Incidents_Filtered
+        
+        # Coloring
+        colorData <- fire_Incidents_Filtered$Fire_Size_Case
+        pal <- colorFactor("YlOrRd", colorData)
+        colorData_nbh <- neighbourhood_Shape$num_incidents
+        pal_nbh <- colorNumeric("Greys", colorData_nbh)
+        
+        log_Estimated <- log(fire_Incidents_Filtered$Estimated_Dollar_Loss) * 10
+        
+        leafletProxy("map", data = fire_Incidents_Filtered) %>% clearShapes() %>%
+            addPolygons(data = neighbourhood_Shape, 
+                        stroke = T, color = "Grey", weight = 1 ,opacity = .2, 
+                        layerId=~AREA_ID,
+                        fillOpacity = 0.2, fillColor=pal_nbh(colorData_nbh)) %>%
+            addCircles(~Longitude, ~Latitude, radius=log_Estimated, layerId=~X_id,
+                       stroke=FALSE, fillOpacity=0.4, fillColor=pal(colorData)) %>% 
+            addCircles(fire_Stations$Longitude, fire_Stations$Latitude, radius=100,
+                       stroke = F,layerId=fire_Stations$ID,
+                       fillOpacity = 0.2, fillColor = "blue")
+    })
+    
+    # Reset the view of the map
+    observeEvent(input$Zoom_Out, {
+        
+        # Reset view
+        leafletProxy("map") %>% 
+            setView(lng = -79.24, lat = 43.74, zoom = 11)  
+        
+        # Reset neighborhood
+        global_varible$current_neibourhood <- ""
+    })
+    
+    # Plot the scatter plot for given location
+    time_incident_plot <- function(current_data){
+        
+    }
+    
+    # Draw the scatter plot
     output$scatterplot <- renderPlot({
-        ggplot(mtcars, aes(x=wt, y=mpg)) + 
-            geom_point()+ geom_smooth()+ ggtitle("Demo Scatterplot")+theme(
-                plot.title = element_text(color="black", size=14, face="bold")
-            )
+        
+        # Get data
+        fire_Incidents_Filtered <- global_varible$fire_incidents_input
+        neibourhood_Filtered <- global_varible$current_neibourhood
+        
+        # Check if any neighborhood is clicked
+        if (neibourhood_Filtered != ""){
+            fire_Incidents_Filtered <- fire_Incidents_Filtered %>% 
+                filter(Neighborhood == neibourhood_Filtered)}
+        
+        # Group by date
+        fire_Incidents_Filtered <- fire_Incidents_Filtered %>%
+            group_by(TFS_Alarm_Month) %>%
+            summarise(count = n()) 
+        
+        # Plot the scatter plot
+        ggplot(fire_Incidents_Filtered, aes(x=TFS_Alarm_Month, y=count)) + 
+                geom_point()+ geom_smooth()+ ggtitle("Demo Scatterplot")+theme(
+                    plot.title = element_text(color="red", size=14, face="bold"))
+        
+    })
+    
+    # Show a popup at the given location
+    showZipcodePopup <- function(zipcode, lat, lng) { 
+        
+        # set popups
+        leafletProxy("map") %>% addPopups(lng, lat, "Popups", layerId = zipcode)
+    
+    }
+    
+    # When map is clicked, show a popup with city info
+    observe({
+        leafletProxy("map") %>% clearPopups()
+        event <- input$map_shape_click
+        if (is.null(event))
+            return()
+        
+        # If neighborhood is clicked
+        if (event$id > 2480000){ 
+            neigbourhoodSelected <- neighbourhood_Shape[which(neighbourhood_Shape$AREA_ID == event$id),]
+            # Add current filtering to the gv
+            global_varible$current_neibourhood <- neigbourhoodSelected$AREA_NAME
+            
+            #Find binding box and change location
+            neigbourhoodSelected <- neigbourhoodSelected$geometry %>% sf::st_bbox()
+            leafletProxy("map") %>% flyToBounds(as.numeric(neigbourhoodSelected$xmin), as.numeric(neigbourhoodSelected$ymin), 
+                                                as.numeric(neigbourhoodSelected$xmax), as.numeric(neigbourhoodSelected$ymax))
+            
+            # If fire incidents is clicked
+        } else if (event$id > 1350000) {
+            # Add current filtering to the gv
+            global_varible$current_incidents <- event$id
+            
+            # If fire stations is clicked
+        } else {
+            # Add current filtering to the gv
+            global_varible$current_incidents <- event$id
+        }
+        
+        isolate({
+            showZipcodePopup(event$id, event$lat, event$lng)
+        })
     })
 
     
--- app/ui.R
+++ app/ui.R
@@ -1,5 +1,6 @@
 source("../init.R")
 library(shiny)
+library(shinyWidgets)
 library(shinyBS)
 library(dplyr)
 library(leaflet)
@@ -26,66 +27,64 @@ shinyUI(
                 h2("City of Toronto fire incidents browser"),
                 
                 # slider for selecting type of fire
-                selectInput("Fire_Type",
-                                "Select fire type",
-                                choices = list("Fire" = 1, "Explosion" = 2, 
-                                               "Outdoor Fire" = 3, "All" = 4), selected = 4),
-                bsTooltip(id = "Fire_Type", title = "Type of fire recorded by the firefighters", 
-                             placement = "bottom", trigger = "hover", options = NULL),
+                pickerInput("Fire_Type",
+                            label = h4("Select fire type ", bsButton("s1", class = "info",  label = "", icon = icon("question"), style = "info", size = "extra-small")),
+                                choices = fire_Type_Choice, multiple = TRUE, selected = 1,
+                                options = list(`actions-box` = TRUE)),
+                bsPopover(id = "s1", title = "Casualties", placement = "top", content =  "Working on it..."),
 
                     
                 # slider for selecting the size of fire
-                selectInput("Fire_Size",
-                                       "Select fire size",
-                                       choices = list("Extinguished" = 1, "Small" = 2, 
-                                                      "Medium" = 3, "Large" = 4, "All" = 5),selected = 5),
-                bsTooltip(id = "Fire_Size", title = "Size of fire when firefighters arrrived at scene", 
-                              placement = "bottom", trigger = "hover", options = NULL),
+                pickerInput("Fire_Size",
+                            label = h4("Select fire size", bsButton("s2", class = "info", label = "", icon = icon("question"), style = "info", size = "extra-small")),
+                            choices = fire_Size_Choice, multiple = TRUE, selected = 1,
+                            options = list(`actions-box` = TRUE)),
+                bsPopover(id = "s2", title = "Casualties",placement = "top",content =  "Working on it..."),
 
                     
-                # slider for selecting the orgin of fire
-                selectInput("Area_Origin",
-                                "Select estimated orgin of fire",
-                                choices = list("Cooking" = 1, "Engine" = 2, 
-                                               "Trash Storage" = 3, "Parch or Balcony" = 4,
-                                               "Others" = 5, "All" = 6), selected = 6),
-                bsTooltip(id = "Area_Origin", title = "The estimated orgin of fire", 
-                              placement = "bottom", trigger = "hover", options = NULL),
-                    
+                # slider for selecting the origin of fire
+                pickerInput("Area_Origin",
+                            label = h4("Orgin of fire", bsButton("s3", class = "info", label = "", icon = icon("question"), style = "info", size = "extra-small")),
+                            choices = area_Origin_Choice, multiple = TRUE, selected = 1,
+                            options = list(`actions-box` = TRUE)),
+                bsPopover(id = "s3", title = "Casualties", placement = "top",content =  "Working on it..."),
+
                 # slider for selecting financial loss
-                shinyWidgets::sliderTextInput("Financial_Loss",
-                                "Select range of financial loss",
+                sliderTextInput("Financial_Loss",
+                                label = h4("Financial loss", bsButton("s4", class = "info", label = "", icon = icon("question"), style = "info", size = "extra-small")),
                                 choices=c(0, 10, 100, 1000, 10000,100000, 1000000),
                                 selected = c(10,10000), grid = T),
-                bsTooltip(id = "Financial_Loss", title = "The estimated amount of financial loss", 
-                              placement = "bottom", trigger = "hover", options = NULL),
+                bsPopover(id = "s4", title = "Financial Loss",placement = "top", content =  "Working on it..."),
                     
                 # slider for selecting number of casualties
                 sliderInput("Casualties",
-                                "Number of civillian injured or dead",
+                            label = h4("Casualties", bsButton("s5", class = "info", label = "", icon = icon("question"), style = "info", size = "extra-small")),
                                 min = 0, max = 15,
                                 value = 0),
-                bsTooltip(id = "Casualties", title = "Number of civillian injured or dead", 
-                              placement = "bottom", trigger = "hover", options = NULL),
+                bsPopover(id = "s5", title = "Casualties", placement = "top",content =  "Working on it..."),
                     
                 # slider for selecting number of personnel
                 shinyWidgets::sliderTextInput("Num_Personnel",
-                                "Number of firefighters arrivaled at the scene",
+                                label = h4("firefighters at scene", bsButton("s6", class = "info", label = "", icon = icon("question"), style = "info", size = "extra-small")),
                                 choices=c(0, 5, 10, 20, 35, 50, 100),
                                 selected = c(0,10), grid = T),
-                bsTooltip(id = "Num_Personnel", title = "Number of firefighters arrivaled at the scene", 
-                              placement = "bottom", trigger = "hover", options = NULL),
+                bsPopover(id = "s6", title = "Casualties", placement = "top",content =  "Working on it..."),
                 
                 # Date input
-                dateRangeInput("Time","Select time period"),
-                bsTooltip(id = "Time", title = "Incidents that occurred during this period of time", 
-                              placement = "bottom", trigger = "hover", options = NULL)
+                dateRangeInput("Time",
+                               label = h4("Select time period", bsButton("s7", class = "info", label = "", icon = icon("question"), style = "info", size = "extra-small")),
+                               "2011-01-01", "2019-07-01"),
+                bsPopover(id = "s7", title = "Casualties", placement = "top", content =  "Working on it..."),
+                
+                # Refresh
+                actionButton("Refresh", "Click to refresh"),
+                actionButton("Zoom_Out", "Return to full view")
         
                 ),
             
             # Sidebars with scatter plot
             absolutePanel(id = "scatterplot", class = "panel panel-default",
-                          fixed = TRUE, draggable = TRUE, top = "auto", left = 20, right = "auto", bottom = 20,
+                          fixed = TRUE, top = "auto", left = 20, right = "auto", bottom = 20,
                           width = "auto", height = "auto",
                           
                           plotOutput("scatterplot", height = 300,  width = 450))
